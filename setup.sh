ed6da11fcdca09ac52828bb2b00576f54941a58f
Pikogimang <111824875+Pikogimang@users.noreply.github.com>
2024-09-25 22:23:07 +0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,19 @@
+cd
+
+echo "Installing dependencies"
+yes | pkg update
+yes | pkg upgrade
+pkg install git -y
+pkg install nodejs -y
+
+echo "Cloning repository"
+git clone https://github.com/FLeafs/x.git x
+
+echo "NPM install"
+cd && cd x
+npm i node-fetch@2.6.1 express
+
+echo "Done... sekarag run :"
+echo ""
+echo "cd && cd x && node index.js"
+cd
